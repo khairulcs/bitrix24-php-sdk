76fb7194cb3a9bb9a326ba00a4b9de4efa863520
Khair <khairul.azwan@carsome.my>
2020-05-03 23:06:42 +0800

    update param

--- index.php
+++ index.php
@@ -8,18 +8,28 @@ use Monolog\Handler\StreamHandler;
 $log = new Logger('bitrix24');
 $log->pushHandler(new StreamHandler('log/error.log', Logger::DEBUG));
 
+$param = array(
+    'scope' => 'app',
+    'app_id' => 'local.5ea23d7a8a67f7.95443029',
+    'app_secret' => 'xO6V1vibY9EaytkukVum0skzxg4Kvci0LfRJgtvVaP4NqoWfjW',
+    'domain' => 'oauth.bitrix.info',
+    'member_id' => '0ee66411b1e9370b0a8adcc65abad92d',
+    'access_token' => '2ce5ae5e0046c6030040b3220000011c5046032ddf5d0602e75fab1adaa8f7c3a752dd',
+    'refresh_token' => '1c64d65e0046c6030040b3220000011c5046033a986a4624bc9d4dcdaed50f38b9ccc8'
+
+);
 
 // init lib
 $obB24App = new \Bitrix24\Bitrix24(false, $log);
-$obB24App->setApplicationScope($arParams['app']);
-$obB24App->setApplicationId($arParams['local.5ea23d7a8a67f7.95443029']);
-$obB24App->setApplicationSecret($arParams['xO6V1vibY9EaytkukVum0skzxg4Kvci0LfRJgtvVaP4NqoWfjW']);
+$obB24App->setApplicationScope($arParams['scope']);
+$obB24App->setApplicationId($arParams['app_id']);
+$obB24App->setApplicationSecret($arParams['app_secret']);
  
 // set user-specific settings
-$obB24App->setDomain($arParams['oauth.bitrix.info']);
-$obB24App->setMemberId($arParams['0ee66411b1e9370b0a8adcc65abad92d']);
-$obB24App->setAccessToken($arParams['2ce5ae5e0046c6030040b3220000011c5046032ddf5d0602e75fab1adaa8f7c3a752dd']);
-$obB24App->setRefreshToken($arParams['1c64d65e0046c6030040b3220000011c5046033a986a4624bc9d4dcdaed50f38b9ccc8']);
+$obB24App->setDomain($arParams['domain']);
+$obB24App->setMemberId($arParams['member_id']);
+$obB24App->setAccessToken($arParams['access_token']);
+$obB24App->setRefreshToken($arParams['refresh_token']);
 
 // get information about current user from bitrix24
 $obB24User = new \Bitrix24\User\User($obB24App);
