dd656b5ab88bc71dc5edd4f4187b50fa30580d09
Khair <khairul.azwan@carsome.my>
2020-05-09 07:44:07 +0800

    session tokens

--- refresh-token.php
+++ refresh-token.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/const/bitrix.php';
 
@@ -22,6 +23,13 @@ $access_token_expired = $obB24App->isAccessTokenExpire();
 // set refresh token
 $obB24App->setRefreshToken($refresh_token);
 
-// renew token
-$renew_token = $obB24App->getNewAccessToken();
+// renew token if expired
+if(!$access_token_expired) {
+    $renew_token = $obB24App->getNewAccessToken();
+    $access_token = $renew_token['access_token'];
+    $refresh_token = $renew_token['refresh_token'];
+}
+$_SESSION['access_token'] = $access_token;
+$_SESSION['refresh_token'] = $refresh_token;
+echo "<a href='refresh-token.php?access_token=".$_SESSION['access_token']."&refresh_token=".$_SESSION['refresh_token']."'>Check expired AT</a>";
 var_dump($renew_token);
