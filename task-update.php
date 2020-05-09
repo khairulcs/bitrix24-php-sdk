c6cb4f1643d34a303581522610df5e2f85db89fb
Khair <khairul.azwan@carsome.my>
2020-05-09 09:38:17 +0800

    update task content

--- task-update.php
+++ task-update.php
@@ -51,12 +51,73 @@ $filter = array(
 $responsible_user = $obB24User->get('name', 'ASC', $filter);
 $responsible_email = $responsible_user['result'][0]['EMAIL'];
 
+// set arrays of card
+
+$events = array(
+    'title' => 'Testing',
+    'body' => 'Testing body',
+);
+
+$wideScreenMode = array(
+    'wide_screen' => false,
+);
+
+$title = array(
+    'tag' => 'plain_text',
+    'content' => $events['title'],
+);
+
+$body = array(
+    'tag' => 'plain_text',
+    'content' => $events['body'],
+);
+
+$header = array(
+    'title' => $title,
+);
+
+$actions = array(
+    array(
+        'tag' => 'button',
+        'text' => array(
+            'tag' => 'plain_text',
+            'content' => 'View in Bitrix24',
+        ),
+        'url' => "https://ramssolgroup.bitrix24.com/company/personal/user/1/tasks/task/view/$task_id",
+        'type' => 'default',
+    ),
+);
+
+$elements = array(
+    array(
+        array(
+            'tag' => 'div',
+            'text' => $body,
+        ),
+        array(
+            'tag' => 'hr',
+        ),
+        array(
+            'tag' => 'action',
+            'actions' => array(
+                $actions,
+            ),
+        ),
+    ),
+);
+$card = array(
+    'config' => $wideScreenMode,
+    'header' => $header,
+    'elements' => $elements,
+);
+
 //writeRaw($_REQUEST, 'incoming');
 // TODO: Notify user in lark
 $data = array(
     'email' => 'khairul.ariffin@feets.me',
     'msg_type' => 'text',
-    'content' => array(
+    'update_multi' => false,
+    'card' => array(
         'text' => $responsible_email,
     ),
 );
