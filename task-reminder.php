9b587aa81662df0a48d82ac25bc09e5d28b1d460
Khair <khairul.azwan@carsome.my>
2020-05-14 17:46:17 +0800

    test task reminder

--- task-reminder.php
+++ task-reminder.php
@@ -37,218 +37,135 @@ $arCurrentB24User = $obB24User->current();
 $obB24TaskItems = new \Bitrix24\Task\Items($obB24App);
 $dateToday = date("Y-m-d");
 $arrOrder = array(
-    'ID' => 'DESC'
+    'ID' => 'DESC',
 );
 $arrFilter = array(
     "<=DEADLINE" => "2020-05-15",
     ">=DEADLINE" => "2020-05-14",
-    "<=REAL_STATUS" => 3
+    "<=REAL_STATUS" => 1,
 );
-$arrTaskData = array("ID","TITLE","DEADLINE","STATUS","RESPONSIBLE_ID");
+$arrTaskData = array("ID", "TITLE", "DEADLINE", "STATUS", "RESPONSIBLE_ID");
 $arrNavParam = array();
 $todayTaskList = $obB24TaskItems->getList($arrOrder, $arrFilter, $arrTaskData);
-echo "<pre>";
-print_r($todayTaskList);die();
-echo "</pre>";
-// get task item
-$task_id = $_REQUEST['data']['FIELDS_AFTER']['ID'];
-// $task_id = 16517;
-$obB24Task = new \Bitrix24\Task\Item($obB24App);
-$arCurrentB24Task = $obB24Task->getData($task_id);
-$responsible_id = $arCurrentB24Task['result']['RESPONSIBLE_ID'];
-$task_title = $arCurrentB24Task['result']['TITLE'];
-$task_desc = $arCurrentB24Task['result']['DESCRIPTION'];
-$task_resp_name = $arCurrentB24Task['result']['RESPONSIBLE_NAME'];
-$task_resp_last_name = $arCurrentB24Task['result']['RESPONSIBLE_LAST_NAME'];
-$task_created_by = $arCurrentB24Task['result']['CREATED_BY'];
-$task_deadline = $arCurrentB24Task['result']['DEADLINE'];
-$task_group_id = $arCurrentB24Task['result']['GROUP_ID'];
+// log the task reminder
+$funcWriteToLog->call($todayTaskList, 'Task Reminder');
+
+$responsible_id = $todayTaskList['result'][0]['RESPONSIBLE_ID'];
+$todayTaskList = $todayTaskList['result'];
 
 // task config
-$header_title = "NEW TASK CREATED";
+$header_title = "REMINDER!";
+$count = 0;
+foreach ($todayTaskList as $key => $value) {
+    $count++;
+    if(($count%10) == 1) {
+        sleep(1);
+    }
+    // get task item
+    $task_id = $value['ID'];
+    $responsible_id = $value['RESPONSIBLE_ID'];
+    $task_title = $value['TITLE'];
+    $task_deadline = $value['DEADLINE'];
 
-// adjust date formate
-$task_deadline = date("d-m-Y H:i:s", strtotime($task_deadline));
+    // adjust date format
+    $task_deadline = date("d-m-Y H:i:s", strtotime($task_deadline));
 
-$task_desc = str_replace("[P]", "", $task_desc);
-$task_desc = str_replace("[/P]", "\n", $task_desc);
+    // get user by id
+    $filter = array(
+        'ID' => $responsible_id,
+    );
+    $responsible_user = $obB24User->get('name', 'ASC', $filter);
+    $responsible_email = $responsible_user['result'][0]['EMAIL'];
 
-$filterGroup = array(
-    'ID' => $task_group_id,
-);
-$workgroups = new \Bitrix24\Sonet\SonetGroup($obB24App);
-$group = $workgroups->get('DESC', $filterGroup);
-$task_group_name = $group['result'][0]['NAME'];
+    // set arrays of card
+    $events = array(
+        'header_title' => $header_title,
+        'title' => $task_title,
+        'body' => $task_desc,
+        'deadline' => $task_deadline,
+        'resp_name' => $task_resp_name,
+    );
 
-// log the REQUEST
-$funcWriteToLog->call($_REQUEST, 'Task Update');
+    $wideScreenMode = array(
+        'wide_screen' => false,
+    );
 
-// get user by id
-$filter = array(
-    'ID' => $responsible_id,
-);
-$responsible_user = $obB24User->get('name', 'ASC', $filter);
-$responsible_email = $responsible_user['result'][0]['EMAIL'];
+    $header_title = array(
+        'tag' => 'plain_text',
+        'content' => $events['header_title'],
+    );
 
-// die if no email in the list
-$search = $responsible_email;
-$lines = file('subscribers.txt');
-// Store true when the text is found
-$found = false;
-foreach($lines as $line)
-{
-  if(strpos($line, $search) !== false)
-  {
-    $found = true;
-    echo $line;
-  }
-}
-// If the text was not found, show a message
-if(!$found)
-{
-  $funcWriteToLog->call($found, 'SEND MESSAGE');
-  die();
-}
+    $combined_body = array(
+        'tag' => 'lark_md',
+        'content' => "**Title:** " . $events['title'] .
+        "\n**Responsible:** " . $events['resp_name'] .
+        "\n**Due Date:** " . $events['deadline'],
+    );
 
-// get created by user
-$filter_created_by = array(
-    'ID' => $task_created_by,
-);
-$created_user = $obB24User->get('name', 'ASC', $filter_created_by);
-$created_name = $created_user['result'][0]['NAME'];
-
-// set arrays of card
-$events = array(
-    'header_title' => $header_title,
-    'title' => $task_title,
-    'body' => $task_desc,
-    'deadline' => $task_deadline,
-    'group_name' => $task_group_name,
-    'resp_name' => $task_resp_name,
-    'created_by' => $created_name,
-);
-
-$wideScreenMode = array(
-    'wide_screen' => false,
-);
-
-$header_title = array(
-    'tag' => 'plain_text',
-    'content' => $events['header_title'],
-);
-
-$combined_body = array(
-    'tag' => 'lark_md',
-    'content' => "**Title:** " . $events['title'] . 
-    "\n**Project:** " . $events['group_name'] . 
-    "\n**Created by:** " . $events['created_by'] . 
-    "\n**Responsible:** " . $events['resp_name'] . 
-    "\n**Due Date:** " . $events['deadline'] . 
-    "\n**Details:**\n" . $events['body'],
-);
-
-$title = array(
-    'tag' => 'lark_md',
-    'content' => "**Title:** " . $events['title'],
-);
-
-$created = array(
-    'tag' => 'lark_md',
-    'content' => "**Created by:** " . $events['created_by'],
-);
-
-$responsible = array(
-    'tag' => 'lark_md',
-    'content' => "**Responsible:** " . $events['resp_name'],
-);
-
-$project = array(
-    'tag' => 'lark_md',
-    'content' => "**Project:** " . $events['group_name'],
-);
-
-$body = array(
-    'tag' => 'lark_md',
-    'content' => "**Details:**\n" . $events['body'],
-);
-
-$deadline = array(
-    'tag' => 'lark_md',
-    'content' => "**Due Date:** " . $events['deadline'],
-);
-
-$header = array(
-    'title' => $header_title,
-);
-
-$actions = array(
-    array(
-        'tag' => 'button',
-        'text' => array(
-            'tag' => 'plain_text',
-            'content' => 'View in Bitrix24',
+    $actions = array(
+        array(
+            'tag' => 'button',
+            'text' => array(
+                'tag' => 'plain_text',
+                'content' => 'View in Bitrix24',
+            ),
+            'url' => "https://ramssolgroup.bitrix24.com/company/personal/user/$responsible_id/tasks/task/view/$task_id/",
+            'type' => 'default',
         ),
-        'url' => "https://ramssolgroup.bitrix24.com/company/personal/user/$responsible_id/tasks/task/view/$task_id/",
-        'type' => 'default',
-    ),
-);
+    );
 
-$elements2 = array(
-    array(
-        'tag' => 'div',
-        'text' => $combined_body,
-    ),
-    array(
-        'tag' => 'hr',
-    ),
-    array(
-        'tag' => 'action',
-        'actions' => $actions,
-    ),
-);
+    $elements2 = array(
+        array(
+            'tag' => 'div',
+            'text' => $combined_body,
+        ),
+        array(
+            'tag' => 'hr',
+        ),
+        array(
+            'tag' => 'action',
+            'actions' => $actions,
+        ),
+    );
 
-$elements = array(
-    array(
-        'tag' => 'div',
-        'text' => $title,
-    ),
-    array(
-        'tag' => 'div',
-        'text' => $project,
-    ),
-    array(
-        'tag' => 'div',
-        'text' => $deadline,
-    ),
-
-    array(
-        'tag' => 'div',
-        'text' => $body,
-    ),
-    array(
-        'tag' => 'hr',
-    ),
-    array(
-        'tag' => 'action',
-        'actions' => $actions,
-    ),
-);
-$card = array(
-    'config' => $wideScreenMode,
-    'header' => $header,
-    'elements' => $elements2,
-);
+    $card = array(
+        'config' => $wideScreenMode,
+        'header' => $header,
+        'elements' => $elements2,
+    );
 
 //writeRaw($_REQUEST, 'incoming');
-// TODO: Notify user in lark
-$data = array(
-    'email' => 'khairul.ariffin@feets.me',
-    'msg_type' => 'interactive',
-    'update_multi' => false,
-    'card' => $card,
-);
-$payload = json_encode($data);
-$funcSendMessage = new message();
-$send = $funcSendMessage->send($app_access_token, $payload);
+    // TODO: Notify user in lark
+    $data = array(
+        'email' => 'khairul.ariffin@feets.me',
+        'msg_type' => 'interactive',
+        'update_multi' => false,
+        'card' => $card,
+    );
+    $payload = json_encode($data);
+    $funcSendMessage = new message();
+    $send = $funcSendMessage->send($app_access_token, $payload);
 
-$funcWriteToLog->call($send, 'SEND MESSAGE');
+    $funcWriteToLog->call($send, 'SEND MESSAGE');
+}
+
+// // $task_id = 16517;
+// $obB24Task = new \Bitrix24\Task\Item($obB24App);
+// $arCurrentB24Task = $obB24Task->getData($task_id);
+
+// // die if no email in the list
+// $search = $responsible_email;
+// $lines = file('subscribers.txt');
+// // Store true when the text is found
+// $found = false;
+// foreach ($lines as $line) {
+//     if (strpos($line, $search) !== false) {
+//         $found = true;
+//         echo $line;
+//     }
+// }
+// // If the text was not found, show a message
+// if (!$found) {
+//     $funcWriteToLog->call($found, 'SEND MESSAGE');
+//     die();
+// }
